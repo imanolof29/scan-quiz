e3dc8586a7a628537b322749e70dbe6f14d778f8
Imanol Ortiz <imanolof29@gmail.com>
2026-03-23 11:15:53 +0100

    some fixes

--- src/notifications/notifications.module.ts
+++ src/notifications/notifications.module.ts
@@ -3,11 +3,12 @@ import { NotificationsService } from "./notifications.service";
 import { NotificationsController } from "./notifications.controller";
 import { NotificationTokenEntity } from "./entity/notification-token";
 import { TypeOrmModule } from "@nestjs/typeorm";
+import { AuthModule } from "../auth/auth.module";
 
 @Module({
     providers: [NotificationsService],
     exports: [NotificationsService],
     controllers: [NotificationsController],
-    imports: [TypeOrmModule.forFeature([NotificationTokenEntity]),]
+    imports: [TypeOrmModule.forFeature([NotificationTokenEntity]), AuthModule]
 })
 export class NotificationsModule { }
